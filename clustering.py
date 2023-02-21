9e361ae06e179ec71cc01e4cfb670197d78a1f18
abdelhak.chahid <ch_abdelhak@hotmail.com>
2023-02-21 15:46:22 +0100

    adjusting code

--- clustering.py
+++ clustering.py
@@ -756,7 +756,7 @@ if st.session_state["authentication_status"]:
     selected_option = option_to_model(select_box,option_models)
     num_clusters = ste.number_input(label = 'Insert amount of clusters', min_value = min_value, max_value = max_value)
     num_clusters = int(num_clusters)
-    ste.write('Amount of clusters is ', num_clusters)
+    st.write('Amount of clusters is ', num_clusters)
 
     load_transformers = ste.button('GENERATE CLUSTERS: TRANSFORMERS')
 
